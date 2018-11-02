38340dae4907745499768327d628c6c55ce61874
Gabe Abrams <gabeabrams@gmail.com>
2018-11-02 12:04:36 -0400

    Separated out Validator

--- Validator.js
+++ Validator.js
@@ -1,38 +1,94 @@
-const genSignature = require('./genSignature.js');
+const oauth = require('oauth-signature');
+const clone = require('fast-clone');
+const urlLib = require('url');
+
 const MemoryNonceStore = require('./MemoryNonceStore.js');
 
+/* LTI launch validator */
 class Validator {
-  constructor(options) {
-    options = options || {};
-    this.nonceStore = options.nonceStore || new MemoryNonceStore();
+  /**
+   * Creates a new Validator
+   * @param {string} consumerKey - an LTI consumer id to compare against during
+   *   launch validation
+   * @param {string} consumerSecret - an LTI consumer secret to use for
+   *   signature signing
+   * @param {object} [nonceStore=memory store] - a nonce store to use for
+   *   keeping track of used nonces
+   */
+  constructor(config = {}) {
+    this.nonceStore = config.nonceStore || new MemoryNonceStore();
 
     // Consumer credentials
-    this.consumerKey = options.consumerKey;
-    this.consumerSecret = options.consumerSecret;
-    if (!this.consumerKey || !this.consumerSecret) {
-      throw new Error('Validator requires consumerKey and consumerSecret');
+    if (!config.consumerSecret) {
+      throw new Error('Validator requires consumerSecret');
     }
+    this.consumerSecret = config.consumerSecret;
+    if (!config.consumerKey) {
+      throw new Error('Validator requires consumerKey');
+    }
+    this.consumerKey = config.consumerKey;
   }
 
   /**
-   * Checks of an LTI launch request is valid
-   * @param {object} req - Express request object
-   * @return Promise that resolves if valid, rejects if invalid
+   * Checks if an LTI launch request is valid
+   * @param {object} req - Express request object to verify
+   * @return {Promise} promise that resolves if valid, rejects if invalid
    */
   isValid(req) {
+    // Check that consumerKey is valid
+    if (
+      !req.body
+      || !req.body.oauth_consumer_key
+      || req.body.oauth_consumer_key !== this.consumerKey
+    ) {
+      // No consumer key or consumer key didn't match (reject immediately)
+      return Promise.reject();
+    }
+    // Check that nonce and signature are valid
+    return this._checkNonce(req)
+      .then(() => {
+        if (!this._isSignatureValid(req)) {
+          throw new Error('Invalid signature!');
+        }
+      });
+  }
 
-    return new Promise((resolve, reject) => {
-      // Check signature
-      const generatedSignature = genSignature(req, this.consumerSecret);
-      console.log('Our generated', generatedSignature);
-      if (generatedSignature !== req.body.oauth_signature) {
-        // Invalid
-        return reject(new Error('Invalid signature!'));
-      }
+  /**
+   * Checks if a nonce is valid
+   * @param {object} req - Express request object to verify
+   * @return Promise that resolves if valid, rejects if invalid
+   */
+  _checkNonce(req) {
+    return this.nonceStore.check(
+      req.body.oauth_nonce,
+      req.body.oauth_timestamp
+    );
+  }
 
-      // Check nonce
-      return this.nonceStore.check(req.body.oauth_nonce, req.body.oauth_timestamp);
-    });
+  /**
+   * Checks if an oauth_signature is valid
+   * @param {object} req - Express request object to verify
+   * @return boolean, true if req.body.oauth_signature is valid
+   */
+  _isSignatureValid(req) {
+    // Generate signature for verification
+    // > Build URL
+    const path = urlLib.parse(req.originalUrl || req.url).pathname;
+    const url = req.protocol + '://' + req.headers.host + path;
+    // > Remove oauth signature from body
+    const body = clone(req.body);
+    delete body.oauth_signature;
+    // > Create signature
+    const generatedSignature = decodeURIComponent(
+      oauth.generate(
+        req.method,
+        url,
+        body,
+        this.consumerSecret
+      )
+    );
+
+    return (generatedSignature === req.body.oauth_signature);
   }
 }
 
--- index.js
+++ index.js
@@ -1,73 +1,173 @@
-const MemoryNonceStore = require('./MemoryNonceStore.js');
-const oauth = require('oauth-signature');
-const clone = require('fast-clone');
-const urlLib = require('url');
+const Validator = require('./Validator');
 
-class Validator {
-
-  constructor(options = {}) {
-    this.nonceStore = options.nonceStore || new MemoryNonceStore();
-
-    // Consumer credentials
-    if (!options.consumerSecret) {
-      throw new Error('Validator requires consumerSecret');
-    }
-    this.consumerSecret = options.consumerSecret;
+/**
+ * Returns parsed value of val if val is truthy, otherwise just returns val
+ * @param val - value to parse if truthy
+ * @return value (parsed as int if truthy)
+ */
+const parseIntIfTruthy = (val) => {
+  if (val) {
+    return parseInt(val);
   }
+  return val;
+};
 
-  /**
-   * Checks if an LTI launch request is valid
-   * @param {object} req - Express request object to verify
-   * @return Promise that resolves if valid, rejects if invalid
-   */
-  isValid(req) {
-
-    return this._checkNonce(req)
-    .then(() => {
-      if (!this._isSignatureValid(req)) {
-        throw new Error('Invalid signature!');
-      }
-    });
-
+/**
+ * Returns split array of val if val is truthy, otherwise just returns val
+ * @param val - value to split if truthy
+ * @return value (split on "," if truthy)
+ */
+const splitIfTruthy = (val) => {
+  if (val) {
+    return val.split(',');
   }
+  return val;
+};
 
-  /**
-   * Checks if a nonce is valid
-   * @param {object} req - Express request object to verify
-   * @return Promise that resolves if valid, rejects if invalid
-   */
-  _checkNonce(req) {
-    return this.nonceStore.check(
-      req.body.oauth_nonce,
-      req.body.oauth_timestamp
-    );
+/**
+ * Checks if a value is found in a list
+ * @param [val] - value to search for
+ * @param {array} [list] - the list to search through
+ * @return true if the value is found
+ */
+const valueInList = (val, list) => {
+  if (!val || !list) {
+    return false;
   }
+  return (list.indexOf(val) >= 0);
+};
 
-  /**
-   * Checks if an oauth_signature is valid
-   * @param {object} req - Express request object to verify
-   * @return boolean, true if req.body.oauth_signature is valid
-   */
-  _isSignatureValid(req) {
-    // Generate signature for verification
-    // > Build URL
-    const path = urlLib.parse(req.originalUrl || req.url).pathname;
-    const url = req.protocol + '://' + req.headers.host + path;
-    // > Remove oauth signature from body
-    let body = clone(req.body);
-    delete body.oauth_signature;
-    // > Create signature
-    const generatedSignature = decodeURIComponent(
-      oauth.generate(
-        req.method,
-        url,
-        body,
-        this.consumerSecret
-      )
-    );
+// List of Canvas custom param names
+const CANVAS_CUSTOM_PARAMS = [
+  'custom_canvas_api_domain',
+  'custom_canvas_course_id',
+  'custom_canvas_enrollment_state',
+  'custom_canvas_user_id',
+  'custom_canvas_user_login_id',
+  'custom_canvas_workflow_state',
+];
 
-    return (generatedSignature === req.body.oauth_signature);
-  }
-}
+/**
+ * Create a new validator and sets up route for launch validation and lti
+ *   launch information extraction
+ * @param {object} app - express app to add routes to
+ * @param {string} consumerKey - an LTI consumer key to compare against during
+ *   launch validation
+ * @param {string} [launchPath=/launch] - the express path to catch POST launch
+ *   requests from
+ * @param {string} [redirectToAfterLaunch=same as launchPath] - the path to
+ *   redirect to after a successful launch
+ * @param {string} consumerSecret - an LTI consumer secret to use for
+ *   signature signing
+ * @param {object} [nonceStore=memory store] - a nonce store to use for
+ *   keeping track of used nonces
+ */
+module.exports = (config) => {
+  // Create validator
+  const validator = new Validator({
+    consumerKey: config.consumerKey,
+    consumerSecret: config.consumerSecret,
+    nonceStore: config.nonceStore,
+  });
 
-module.exports = Validator;
+  // Set up launch paths
+  const launchPath = config.launchPath || '/launch';
+  const redirectToAfterLaunch = config.redirectToAfterLaunch || launchPath;
+
+  // Add route for launch validation
+  config.app.post(launchPath, (req, res) => {
+    // Validate the launch request
+    validator.isValid(req)
+      .then(() => {
+        // This is a valid launch request
+
+        // Parse launch and save it to session
+        req.session.launchInfo = {
+          timestamp: req.body.oauth_timestamp,
+          contextId: req.body.context_id,
+          contextLabel: req.body.contextLabel,
+          canvasHost: req.body.custom_canvas_api_domain,
+          courseId: parseIntIfTruthy(req.body.custom_canvas_course_id),
+          enrollmentState: req.body.custom_canvas_enrollment_state,
+          userId: parseIntIfTruthy(req.body.custom_canvas_user_id),
+          userLoginId: req.body.custom_canvas_user_login_id,
+          workflowState: req.body.custom_canvas_workflow_state,
+          extRoles: splitIfTruthy(req.body.ext_roles),
+          launchPresentationTarget:
+            req.body.launch_presentation_document_target,
+          iframeWidth: req.body.launch_presentation_width,
+          iframeHeight: req.body.launch_presentation_height,
+          locale: req.body.launch_presentation_locale,
+          returnURL: req.body.launch_presentation_return_url,
+          userEmail: req.body.lis_person_contact_email_primary,
+          userLastName: req.body.lis_person_name_family,
+          userFullName: req.body.lis_person_name_full,
+          userFirstName: req.body.lis_person_name_given,
+          launchAppTitle: req.body.resource_link_title,
+          roles: splitIfTruthy(req.body.roles),
+          canvasInstance: req.body.tool_consumer_instance_name,
+          userImage: req.body.user_image,
+        };
+
+        // Add simpler role booleans
+        req.session.launchInfo.isInstructor = valueInList(
+          'urn:lti:role:ims/lis/Instructor',
+          req.session.launchInfo.extRoles
+        );
+        req.session.launchInfo.isTA = valueInList(
+          'urn:lti:role:ims/lis/TeachingAssistant',
+          req.session.launchInfo.extRoles
+        );
+        req.session.launchInfo.isDesigner = valueInList(
+          'urn:lti:role:ims/lis/ContentDeveloper',
+          req.session.launchInfo.extRoles
+        );
+        req.session.launchInfo.isCreditLearner = valueInList(
+          'urn:lti:role:ims/lis/Learner',
+          req.session.launchInfo.extRoles
+        );
+        req.session.launchInfo.isNonCreditLearner = valueInList(
+          'urn:lti:role:ims/lis/Learner/NonCreditLearner',
+          req.session.launchInfo.extRoles
+        );
+        req.session.launchInfo.isLearner = (
+          req.session.launchInfo.isCreditLearner
+          || req.session.launchInfo.isNonCreditLearner
+        );
+
+        // Add custom parameters
+        req.session.launchInfo.customParams = {};
+        Object.keys(req.body).forEach((prop) => {
+          // Check if this is a custom param that wasn't sent by Canvas itself
+          if (
+            !prop.startsWith('custom_')
+            || CANVAS_CUSTOM_PARAMS.indexOf(prop) >= 0
+          ) {
+            // Not a custom parameter. Skip!
+            return;
+          }
+          // Save custom parameter
+          req.session.launchInfo.customParams[prop] = req.body[prop];
+        });
+
+        // Save session
+        return new Promise((resolve, reject) => {
+          req.session.save((err) => {
+            if (err) {
+              return reject(err);
+            }
+            return resolve();
+          });
+        });
+      })
+      .then(() => {
+        // Session saved! Now redirect.
+        return res.redirect(redirectToAfterLaunch);
+      })
+      .catch(() => {
+        // Invalid launch request or an error occurred while validating/parsing
+        // launch request
+        return res.status(403).send('We couldn\'t validate your authorization to use this app. Please try launch the app again. If you continue to have problems, please contact an admin.');
+      });
+  });
+};
