31b9573f1389b4cb50aa568128ffa7f98ecac2a9
Gabe Abrams <gabeabrams@gmail.com>
2024-07-15 14:05:19 -0400

    Shortened app id lifespan

--- src/shared/constants/APP_ID_LIFESPAN_SEC.ts
+++ src/shared/constants/APP_ID_LIFESPAN_SEC.ts
@@ -5,7 +5,7 @@
 const APP_ID_LIFESPAN_SEC = (
   process.env.APP_ID_LIFESPAN_SEC
     ? Number.parseFloat(process.env.APP_ID_LIFESPAN_SEC)
-    : 2 * 86400 // 2 days
+    : 3600000 // 1 hour
 );
 
 export default APP_ID_LIFESPAN_SEC;
